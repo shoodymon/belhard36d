4032999918cd2cda0943e47c1ac1271b190075da
shoodymon <shoody2016@yandex.ru>
2022-12-22 23:27:39 +0300

    3_3

---
+++ 3_4.py
@@ -0,0 +1,30 @@
+# Пользователь вводит 3 числа, сказать сколько из них положительных
+# и сколько отрицательных
+
+# Вариант решения задачи через условный оператор if
+number1 = int(input('Введите первое число: '))
+number2 = int(input('Введите второе число: '))
+number3 = int(input('Введите третье число: '))
+count_of_positive_numbers = 0
+count_of_negative_numbers = 0
+if number1 > 0:
+    count_of_positive_numbers += 1
+else:
+    count_of_negative_numbers += 1
+if number2 > 0:
+    count_of_positive_numbers += 1
+else:
+    count_of_negative_numbers += 1
+if number3 > 0:
+    count_of_positive_numbers += 1
+else:
+    count_of_negative_numbers += 1
+print('Положительных чисел: ', count_of_positive_numbers)
+print('Отрицательных чисел: ', count_of_negative_numbers)
+
+# Решение задачи
+number_1 = int(input('Введите первое число: '))
+number_2 = int(input('Введите второе число: '))
+number_3 = int(input('Введите третье число: '))
+check = (number_1 > 0) + (number_2 > 0) + (number_3 > 0)
+print(f"Кол-во положительных чисел: {check}. Кол-во отрицательных чисел: {3 - check}")
