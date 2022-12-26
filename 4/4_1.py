f020b8d01f227b811dc06f74c6aa025c1e74d6c7
shoodymon <shoody2016@yandex.ru>
2022-12-26 16:30:52 +0300

    task 4_1

--- 4/4_1.py
+++ 4/4_1.py
@@ -1,5 +1,5 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n)
 
 number = int(input())
-answer = {2**i for i in range (1, number)}
+answer = {2**i for i in range (1, number+1)}
 print(answer)
