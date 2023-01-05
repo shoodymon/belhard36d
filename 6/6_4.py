67a9fc4fa36d0eae0e152358590c74b077d036b4
shoodymon <shoody2016@yandex.ru>
2023-01-05 13:30:50 +0300

    task with list

---
+++ 6/6_4.py
@@ -0,0 +1,14 @@
+# Дан список содержащий в себе различные типы данных, отфильтровать таким
+# образом, чтобы остались только строки, использование дополнительного списка
+# незаконно
+
+collection = [1, 2, 3, 'hello', 21.42, False, 'python', 1, 3, 'world', 1, 3, 'pycharm']
+print(collection)
+
+def foo(collection):
+    for elements in collection:
+        flag = isinstance(elements, str)
+        if flag:
+            print(elements, end= ' ')
+
+foo(collection)
